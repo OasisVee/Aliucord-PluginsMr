3615c6bb8a495dae1cb0698022540db2f3365c22
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-25 01:46:15 -0400

    Update BetterDashless.java

--- BetterDashless/src/main/java/com/github/MrAn0nym/BetterDashless.java
+++ BetterDashless/src/main/java/com/github/MrAn0nym/BetterDashless.java
@@ -2,7 +2,8 @@ package com.github.MrAn0nym;
 
 import android.content.Context;
 import android.widget.TextView;
-import com.aliucord.Utils;
+import androidx.annotation.NonNull;
+
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.Hook;
@@ -15,45 +16,73 @@ import com.discord.widgets.home.WidgetHome;
 import com.discord.widgets.home.WidgetHomeHeaderManager;
 import com.discord.widgets.home.WidgetHomeModel;
 
+import java.lang.reflect.Method;
+
 @AliucordPlugin
-@SuppressWarnings("unused")
 public class BetterDashless extends Plugin {
-	
-	@Override
-	public void start(Context context) {
-		
-		patcher.patch(ItemChannelText.class, "onConfigure",
-				new Class<?>[]{int.class, ChannelListItem.class}, new Hook(callFrame -> {
-					ItemChannelText _this = (ItemChannelText) callFrame.thisObject;
-					
-					WidgetChannelsListItemChannelBinding binding = null;
-					try {
-						binding = (WidgetChannelsListItemChannelBinding) ReflectUtils
-								.getField(_this, "binding");
-					} catch (NoSuchFieldException | IllegalAccessException e) {
-						e.printStackTrace();
-					}
-					
-					assert binding != null;
-					TextView channelName = binding.getRoot()
-							.findViewById(Utils.getResId("channels_item_channel_name", "id"));
-					
-					channelName.setText(channelName.getText().toString().replace("-", " "));
-				}));
-		
-		patcher.patch(WidgetHomeHeaderManager.class, "configure",
-				new Class<?>[]{WidgetHome.class, WidgetHomeModel.class, WidgetHomeBinding.class},
-				new Hook(callFrame -> {
-					WidgetHomeHeaderManager _this = (WidgetHomeHeaderManager) callFrame.thisObject;
-					WidgetHome widgetHome = (WidgetHome) callFrame.args[0];
-					
-					widgetHome.setActionBarTitle(
-							((WidgetHomeModel) callFrame.args[1]).getChannel().m().replace("-", " "));
-				}));
-	}
-	
-	@Override
-	public void stop(Context context) {
-		patcher.unpatchAll();
-	}
+    
+    @Override
+    public void start(@NonNull Context context) {
+        // Patch channel list item to replace dashes with spaces
+        patcher.patch(ItemChannelText.class, "onConfigure",
+                new Class<?>[]{ int.class, ChannelListItem.class }, 
+                new Hook(callFrame -> {
+                    try {
+                        ItemChannelText itemChannelText = (ItemChannelText) callFrame.thisObject;
+                        WidgetChannelsListItemChannelBinding binding = 
+                            (WidgetChannelsListItemChannelBinding) ReflectUtils
+                                .getField(itemChannelText, "binding");
+                        
+                        TextView channelNameView = binding.getRoot()
+                            .findViewById(com.aliucord.Utils.getResId("channels_item_channel_name", "id"));
+                        
+                        if (channelNameView != null) {
+                            String originalText = channelNameView.getText().toString();
+                            channelNameView.setText(originalText.replace("-", " "));
+                        }
+                    } catch (Exception e) {
+                        com.aliucord.Logger.log(e);
+                    }
+                }));
+        
+        // Patch home header to replace dashes with spaces
+        patcher.patch(WidgetHomeHeaderManager.class, "configure",
+                new Class<?>[]{ WidgetHome.class, WidgetHomeModel.class, WidgetHomeBinding.class },
+                new Hook(callFrame -> {
+                    try {
+                        WidgetHome widgetHome = (WidgetHome) callFrame.args[0];
+                        WidgetHomeModel homeModel = (WidgetHomeModel) callFrame.args[1];
+                        
+                        // Get the channel object
+                        Object channel = homeModel.getChannel();
+                        
+                        // Use reflection to find a method that returns the channel name
+                        String channelName = null;
+                        for (Method method : channel.getClass().getDeclaredMethods()) {
+                            if (method.getReturnType() == String.class && method.getParameterCount() == 0) {
+                                method.setAccessible(true);
+                                try {
+                                    String potentialName = (String) method.invoke(channel);
+                                    if (potentialName != null && potentialName.contains("-")) {
+                                        channelName = potentialName.replace("-", " ");
+                                        break;
+                                    }
+                                } catch (Exception ignored) {}
+                            }
+                        }
+                        
+                        // If we found a name, set it
+                        if (channelName != null) {
+                            widgetHome.setActionBarTitle(channelName);
+                        }
+                    } catch (Exception e) {
+                        com.aliucord.Logger.log(e);
+                    }
+                }));
+    }
+    
+    @Override
+    public void stop(@NonNull Context context) {
+        patcher.unpatchAll();
+    }
 }
